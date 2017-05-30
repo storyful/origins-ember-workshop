6461bca45a1ca4422e95ea803c36bbdc9a750ccb
Bruno Tarmann <bruno@tarmann.com.br>
2017-05-30 11:11:05 +0100

    Config build preferences

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,12 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      extension: 'sass'
+    },
+    'ember-cli-babel': {
+      includePolyfill: true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
